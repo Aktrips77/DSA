c1687ddd3b7dcfc54e8ad92abe40a1e663d0c13b
Aktrips77 <vijaytripathi326@gmail.com>
2026-07-29 23:48:18 +0530

    Time: 3 ms (86.17%) | Memory: 42.8 MB (98.36%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class Solution { 
+    public boolean isValid(String s) { 
+        Stack<Character> st = new Stack<>(); 
+        
+        for (int i = 0; i < s.length(); i++) { 
+            char ch = s.charAt(i); 
+            
+            if (ch == '(' || ch == '{' || ch == '[') { 
+                st.push(ch); 
+            } else { 
+                if (!st.isEmpty()) { 
+                    // Changed ch.peek() to st.peek() and fixed logic
+                    if ((ch == ')' && st.peek() == '(') || 
+                        (ch == '}' && st.peek() == '{') || 
+                        (ch == ']' && st.peek() == '[')) { 
+                        st.pop(); 
+                    } else {
+                        return false; 
+                    }
+                } else {
+                    return false; 
+                }
+            } 
+        } 
+        return st.isEmpty(); 
+    } 
+}
